4da6f03a96a243ea9181c680d089593a29f54b19
Jason Hoekstra <jasonhoekstra@gmail.com>
2013-08-03 20:04:11 -0400

    adding solid resource and now with alignments

--- Insert.py
+++ Insert.py
@@ -4,6 +4,8 @@ import os
 import sys
 import glob
 import logging
+import datetime
+from dateutil.parser import parse
 
 logging.basicConfig(filename='example.log',level=logging.DEBUG)
 
@@ -12,6 +14,7 @@ class lrmi_ETL_insert:
     def __init__(self, directory, host="localhost", user="lri", password="lri", database="lri"):
         self.dir = directory
         self.mysql = mdb.connect(host, user, password, database)
+        self.mysql.set_character_set('utf8')
 
     def run(self):
         """Manages other functions"""
@@ -21,7 +24,9 @@ class lrmi_ETL_insert:
         for fi in files:
             data = self.j_read(fi)
             resource = self.fields(data)
-            ##self.insert(resource)
+
+            if resource != {}:
+                self.insert(resource)
 
     def j_read(self, fi):
         """Reads from the JSON files"""
@@ -55,9 +60,27 @@ class lrmi_ETL_insert:
 
         return return_data
 
+    def fmt(self, value):
+        if value == None:
+            return "NULL"
+        elif type(value).__name__ == "datetime":
+            return "'" + value.strftime("%Y-%m-%d") + "'"
+        else:
+            return "'" + value.replace(u"'",u"''") + "'"
+
+    def date_format(self, value):
+        if value != None:
+            try:
+                value = parse(value)
+            except:
+                value = None
+                pass
+        return value
+
+
     def fields(self, j):
         """Extracts the fields from the JSON file"""
-        temp = {
+        field_map = {
             "doc_ID":"EXTERNAL_GUID",
             "name":"NAME",
             "url":"URL",
@@ -66,17 +89,14 @@ class lrmi_ETL_insert:
             "useRightsUrl":"USE_RIGHTS_URL",
             "isBasedOnUrl":"IS_BASED_ON_URL",
             "timeRequired":"TIME_REQUIRED",
-            "inLanguage":"LANGUAGE_ID",
-            "interactivityType":"INTERACTIVITY_ID",
-            "learningResourceType":"LEARNING_RESOURCE_ID",
             "dateCreated":"DATE_CREATED",
-            "dateModified":"DATE_MODIFIED"}
+            "datePublished":"DATE_PUBLISHED",
+            "dateModified":"DATE_MODIFIED",
+            "sourceText":"SOURCE_TEXT"}
         resource = {}
+        resource_alignments = []
 
         try:
-            resource['doc_ID'] = str(j['doc_ID'])
-            doc_id = str(j['doc_ID'])
-
             #self.print_keys(j)
 
             if 'resource_data_description' in j:
@@ -84,6 +104,9 @@ class lrmi_ETL_insert:
                     if 'items' in j['resource_data_description']['resource_data'] and len(j['resource_data_description']['resource_data']['items']) > 0:
                         item = j['resource_data_description']['resource_data']['items'][0]
                         if 'properties' in item:
+                            resource['doc_ID'] = str(j['doc_ID'])
+                            doc_id = str(j['doc_ID'])
+
                             properties = item['properties']
 
                             resource['name'] = self.return_value(properties, 'name')
@@ -91,16 +114,44 @@ class lrmi_ETL_insert:
                                 resource['name'] = '(Unknown)'
                             resource['url'] = self.return_value(properties, 'url')
                             resource['description'] = self.return_value(properties, 'description')
-                            resource['inLanguage'] = self.return_value(properties, 'inLanguage')
-                            resource['dateCreated'] = self.return_value(properties, 'dateCreated')
-                            resource['dateModified'] = self.return_value(properties, 'dateModified')
-                            resource['datePublished'] = self.return_value(properties, 'datePublished')
-                            ##resource['thumbnailUrl'] = self.return_value(properties, 'thumbnailUrl')
-                            resource['publisher'] = self.return_value(properties, 'publisher')
-                            resource['about'] = self.return_value(properties, 'publisher')
                             resource['useRightsUrl'] = self.return_value(properties, 'useRightsUrl')
                             resource['isBasedOnUrl'] = self.return_value(properties, 'isBasedOnUrl')
+                            resource['copyrightYear'] = self.return_value(properties, 'copyrightYear')
+                            ##resource['inLanguage'] = self.return_value(properties, 'inLanguage')
+                            resource['dateCreated'] = self.date_format(self.return_value(properties, 'dateCreated'))
+                            resource['datePublished'] = self.date_format(self.return_value(properties, 'datePublished'))
+                            resource['dateModified'] = self.date_format(self.return_value(properties, 'dateModified'))
+                            resource['sourceText'] = json.dumps(j, indent=2);
 
+                            if 'educationalAlignment' in properties and type(properties['educationalAlignment']) == list:
+                                ##print type ( properties['educationalAlignment'] ).__name__
+
+                                for alignment in properties['educationalAlignment']:
+                                    if 'type' in alignment and len(alignment['type']) > 0 and alignment['type'][0] == "http://schema.org/AlignmentObject":
+                                        if 'properties' in alignment:
+                                            if 'educationalFramework' in alignment['properties']:
+                                                resource_alignment = {}
+                                                if 'educationalFramework' in alignment['properties'] and len(alignment['properties']['educationalFramework']) > 0 and alignment['properties']['educationalFramework'][0] != "":
+                                                    resource_alignment['educationalFramework'] = alignment['properties']['educationalFramework'][0]
+                                                else:
+                                                    resource_alignment['educationalFramework'] = "Unknown"
+
+                                                if 'targetName' in alignment['properties'] and len(alignment['properties']['targetName']) > 0 and alignment['properties']['targetName'][0] != "":
+                                                    resource_alignment['targetName'] = alignment['properties']['targetName'][0]
+                                                else:
+                                                    resource_alignment['targetName'] = "Unknown"
+
+                                                if 'alignmentType' in alignment['properties'] and len(alignment['properties']['alignmentType']) > 0 and alignment['properties']['alignmentType'][0] != "":
+                                                    resource_alignment['alignmentType'] = alignment['properties']['alignmentType'][0]
+                                                else:
+                                                    resource_alignment['alignmentType'] = "Unknown"
+
+
+                                                resource_alignments.append(resource_alignment)
+
+                            ##resource['thumbnailUrl'] = self.return_value(properties, 'thumbnailUrl')
+                            ##resource['publisher'] = self.return_value(properties, 'publisher')
+                            ##resource['about'] = self.return_value(properties, 'publisher')
 
                             """useRightsUrl
                             isBasedOnUrl
@@ -112,10 +163,8 @@ class lrmi_ETL_insert:
                             educationalAlignment
                             educationalRole"""
 
-
-                            if resource['inLanguage'] != None:
-                                print json.dumps(resource, indent=2)
-
+                            ##if resource['inLanguage'] != None:
+                            ##    print json.dumps(resource, indent=2)
 
                 """
                 if 'identity' in j['resource_data_description']:
@@ -123,51 +172,82 @@ class lrmi_ETL_insert:
                     if j['resource_data_description']['identity']['submitter'] == "inBloom Tagger Application <tagger@inbloom.org>":
                         print j['resource_data_description']['identity']
                 """
-
-
-
-            ##print data
-
-            ##data['name'] = str(j['resource_data_description']['resource_data']['items'][0]['properties']['name'][0])
-            #data['url'] = str(j['resource_data_description']['resource_data']['items'][0]['properties']['url'][0])
-            #data['description'] = str(j['resource_data_description']['resource_data']['items'][0]['properties']['description'][0])
-            #data['dateCreated'] = str(j['resource_data_description']['resource_data']['items'][0]['properties']['dateCreated'][0])
-            #data['inLanguage'] = str(j['resource_data_description']['resource_data']['items'][0]['properties']['inLanguage'][0])
         except Exception as ex:
             print "Exception in field extraction: " + str(ex.message)
             pass
         
 
-        f_data = {}
-        #for key in resource:
-        #    f_data[temp[key]] = resource[key]
-        return f_data
+        field_data = {}
+        for key in resource:
+            field_data[field_map[key]] = resource[key]
+        if len(field_data) > 0:
+            field_data['educationalAlignments'] = resource_alignments
+        return field_data
 
     def insert(self, resource):
         """Inserts the fields into their appropriate fields in the database"""
         update = False
         with self.mysql as m:
-            m.execute("SELECT * FROM resources")
+            m.execute('SET NAMES utf8;')
+            m.execute('SET CHARACTER SET utf8;')
+            m.execute('SET character_set_connection=utf8;')
+            m.execute("SELECT ID, EXTERNAL_GUID FROM resources WHERE EXTERNAL_GUID = %s", resource['EXTERNAL_GUID'])
+
             rows = m.fetchall()
             for row in rows:
-                print row
                 if resource['EXTERNAL_GUID'] in row:
-                    print "?"
                     update = True
             if update:
-                for key in resource:
-                    if key != "EXTERNAL_GUID":
-                        m.execute("UPDATE resources SET %s = %s WHERE %s = %s", (key, resource[key], 'EXTERNAL_GUID',  resource['EXTERNAL_GUID']))
+                pass
+                #for key in resource:
+                #    if key != "EXTERNAL_GUID":
+                #        pass
+                #        ##m.execute("UPDATE resources SET %s = %s WHERE %s = %s", (key, resource[key], 'EXTERNAL_GUID',  resource['EXTERNAL_GUID']))
             else:
-                keys = []
-                values = []
-                for key in resource.keys():
-                    keys.append("'" + key + "', ")
-                for val in resource.values():
-                    values.append("'" + val + "', ")
-                print keys
-                print values
-                m.execute("INSERT INTO resources(%s) VALUES(%s)", (keys, values))
+                sql = "INSERT INTO resources (EXTERNAL_GUID, NAME, DESCRIPTION, URL, COPYRIGHT_YEAR, USE_RIGHTS_URL, IS_BASED_ON_URL, DATE_CREATED, DATE_PUBLISHED, SOURCE_TEXT) VALUES "
+                sql = sql + "(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                sql = sql % (self.fmt(resource['EXTERNAL_GUID']), self.fmt(resource['NAME']), self.fmt(resource['DESCRIPTION']), self.fmt(resource['URL']), self.fmt(resource['COPYRIGHT_YEAR']), \
+                    self.fmt(resource['USE_RIGHTS_URL']), self.fmt(resource['IS_BASED_ON_URL']), self.fmt(resource['DATE_CREATED']), self.fmt(resource['DATE_PUBLISHED']), self.fmt(resource['SOURCE_TEXT']))
+
+                m.execute(sql)
+                resource_id = m.lastrowid
+
+                if len(resource['educationalAlignments']) > 0:
+                    for alignment in  resource['educationalAlignments']:
+                        
+                        framework_id = 0
+                        m.execute("SELECT ID, SYNONYM_ID FROM frameworks WHERE NAME = %s", alignment['educationalFramework'])
+                        framework = m.fetchone()
+                        if framework is not None:
+                            if framework[1] is not None:
+                                framework_id = framework[1]
+                            else:
+                                framework_id = framework[0]
+                        else:
+                            m.execute("INSERT INTO frameworks (NAME, URL) VALUES (%s, '')", alignment['educationalFramework'])
+                            framework_id = m.lastrowid
+
+                        standard_id = 0
+                        m.execute("SELECT ID FROM standards WHERE EXTERNAL_ID = %s AND FRAMEWORK_ID = %s", (alignment['targetName'], framework_id))
+                        standard = m.fetchone()
+                        if standard is not None:
+                            standard_id = standard[0]
+                        else:
+                            m.execute("INSERT INTO standards (EXTERNAL_ID, FRAMEWORK_ID) VALUES (%s, %s)", (alignment['targetName'], framework_id))
+                            standard_id = m.lastrowid
+
+                        alignment_type_id = 0
+                        m.execute("SELECT ID FROM alignment_types WHERE NAME = %s", alignment['alignmentType'])
+                        alignment_type = m.fetchone()
+                        if alignment_type is not None:
+                            alignment_type_id = alignment_type[0]
+                        else:
+                            m.execute("INSERT INTO alignment_types (NAME) VALUES (%s)", alignment['alignmentType'])
+                            alignment_type_id = m.lastrowid
+
+                        if standard_id > 0 and alignment_type_id > 0:
+                            m.execute("INSERT INTO alignments (RESOURCE_ID, STANDARD_ID, ALIGNMENT_TYPE_ID) VALUES (%s, %s, %s)", (resource_id, standard_id, alignment_type_id))
+
             m.close()
 
 if __name__ == "__main__":
